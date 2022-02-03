66380df3555dd1b250a083ff9644219289413de8
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-02-02 23:39:53 -0300

    feat: create TransactionsTable component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th> 
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td className="deposit">R$ 1200.00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Alugeul</td>
+            <td className="withdraw">- R$500.00</td>
+            <td>Imovél</td>
+            <td>30/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
