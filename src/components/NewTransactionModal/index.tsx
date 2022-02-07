aa5128330fa08403ef7683835bb7ec89322d4314
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-02-06 23:27:17 -0300

    feat: add state for radiobox

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { Container, TransactionTypeContainer } from './styles'
+import { 
+  Container, 
+  RadioBox,
+  TransactionTypeContainer 
+} from './styles'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,6 +19,9 @@ export default function NewTransactionModal({
   isOpen, 
   onRequestClose 
 }: NewTransactionModalProps) {
+
+  const [type, setType] = useState('deposit'); 
+
   return (
     <Modal
       isOpen={isOpen}
@@ -41,25 +49,31 @@ export default function NewTransactionModal({
         />
 
         <TransactionTypeContainer>
-          <button
+          <RadioBox
             type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada" />
             <span>
               Entrada
             </span>
 
-          </button>
+          </RadioBox>
 
-          <button
+          <RadioBox
             type="button"
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>
               Saída
             </span>
 
-          </button>
+          </RadioBox>
 
         </TransactionTypeContainer>
 
